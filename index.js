ea6adfec59c0f5d0686f1b1f3d7438dd5cff8c2f
leslieyi <leslieyi411@gmail.com>
2021-05-28 12:31:00 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+const event = "birthday";
+
+function writeCards(names, event) {
+    let newArray = []; //declaring new array? for the following strings to push into??? whatt
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+   
+    return newArray
+}
+
+function countDown(n) {
+
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+
+}
+
